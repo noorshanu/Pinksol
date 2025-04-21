dcaa4db9824e134a03bd559b85c19546f5ff7fe6
noor alam <51040978+noorshanu@users.noreply.github.com>
2025-04-21 23:49:20 +0530

    Update AppLayout.jsx

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -75,7 +75,11 @@ function AppLayout() {
           </button> */}
         
         </div>
-        <a href="https://solscan.io/token/CGfu4VXYRRAZL6SLNkVJMQ3f2W8ffPCYYM6yENjW2eWz" target="_blank" data-aos="fade-up" className="bg-[#bc1d5c] my-2 text-white font-bold py-2 px-2 sm:px-16 rounded sm:text-lg text-xs   flex justify-center w-[360px] sm:w-[720px] mx-auto">CA : CGfu4VXYRRAZL6SLNkVJMQ3f2W8ffPCYYM6yENjW2eWz</a>
+        <a href="https://solscan.io/token/" target="_blank" data-aos="fade-up"
+         className="bg-[#bc1d5c] my-2 text-white font-bold py-2 px-2 sm:px-16 rounded sm:text-lg text-xs   
+         flex justify-center w-[360px] sm:w-[720px] mx-auto">
+          CA : soon..
+          </a>
         <audio ref={audioRef} src={Music}></audio>
 
       
